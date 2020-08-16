ba1c3013f7060fada0cbb86c9051b4198336de0a
Ariel Cima <arielcima@Ariels-MBP.home>
2020-08-16 13:17:34 +0200

    tenants stuff

--- src/Components/JamSections/LandlordOverview/Overview/index.js
+++ src/Components/JamSections/LandlordOverview/Overview/index.js
@@ -1,52 +1,43 @@
 import React from 'react';
 import moment from 'moment';
 import isEmpty from 'lodash/isEmpty';
+import OverviewLine from './OverviewLine';
+
+import { setSection, setSubSection } from '../../../../redux/actions/navigateActions';
 
 import './index.scss';
-import { setSection, setSubSection } from '../../../../redux/actions/navigateActions';
 
 const RoomsOverview = ({ roomsFullInfo }) => {
     
     const showOverviewChart = !isEmpty(roomsFullInfo);
+    
 
-    const handleClick = (section, subSection) => {
-        console.log('CLICK :', section, ' / ', subSection)
-        setSection(section);
-        setSubSection(subSection)
-    };
 
     const renderRoomsChart = () => roomsFullInfo.map((room, i) => {
-        
-        const vacantRoom = isEmpty(room.bookings.currentBooking);
-        const bookInfo = room.bookings.currentBooking;
 
         return (
-            <div className="overview-info-line"
-                key={i} 
-                onCLick={() => handleClick('Rooms', room.roomId)}
-            >
-                <div className="overview-info-block">
-                    <p>{room.roomNr}</p>
-                </div>
-                <div className="overview-info-block">
-                    <p>{room.roomType}</p>
-                </div>
-                <div className="overview-info-block">
-                    {vacantRoom ?  <span>vacant</span>: <p>{bookInfo.jammerName}</p>}
-                </div>
-                {/* <div className="overview-info-block">
-                    <p>{moment(room.bookings.currentBooking.checkIn).format('DD-MMM-YYYY')}</p>
-                </div>
-                <div className="overview-info-block">
-                    <p>{moment(room.bookings.currentBooking.checkOut).format('DD-MMM-YYYY')}</p>
-                </div> */}
-                <div className="overview-info-block">
-                    {vacantRoom ? <p>-</p> : <p>{bookInfo.deposit}</p>}
-                </div>
-                <div className="overview-info-block">
-                    {vacantRoom ? <p>-</p> : <p>{bookInfo.rent}</p>}
-                </div>
-            </div>
+
+            <OverviewLine key={i}  room={room} />
+            // <div className="overview-info-line"
+            //     key={i} 
+            //     onCLick={e => handleClick(e, roomId)}
+            // >
+            //     <div className="overview-info-block">
+            //         <p>{room.roomNr}</p>
+            //     </div>
+            //     <div className="overview-info-block">
+            //         <p>{room.roomType}</p>
+            //     </div>
+            //     <div className="overview-info-block">
+            //         {vacantRoom ?  <span>vacant</span>: <p>{bookInfo.jammerName}</p>}
+            //     </div>
+            //     <div className="overview-info-block">
+            //         {vacantRoom ? <p>-</p> : <p>{bookInfo.deposit}</p>}
+            //     </div>
+            //     <div className="overview-info-block">
+            //         {vacantRoom ? <p>-</p> : <p>{bookInfo.rent}</p>}
+            //     </div>
+            // </div>
         )
     });
 
