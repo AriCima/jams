552eb4f4b5e9b0e854a1575e8fc2db01dec08322
Ariel Cima <arielcima@Ariels-MBP.home>
2020-08-29 19:26:17 +0200

    jammers list ready

--- src/Components/JamSections/LandlordTenants/index.js
+++ src/Components/JamSections/LandlordTenants/index.js
@@ -3,17 +3,17 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import DataService from '../../services/DataService';
+import Calculations from '../../services/Calculations';
 import LandlordTenantsList from './LandlordTenantsList';
-import LandlordTenantInfo from './LandlordTenantInfo';
 import { setSubSection } from '../../../redux/actions/navigateActions';
 
 
 import './index.scss';
 
-const LandlordTenants = ({ jamId, subSection, setSubSection }) => {
+const LandlordTenants = ({ jamId, subSection }) => {
     const [jammers, setJammers] = useState([]);
-    const [showJammersList, setShowJammersList] = useState(false);
-    const [jammerInfo, setJammerInfo] = useState({});
+    // const [showJammersList, setShowJammersList] = useState(false);
+    // const [jammerInfo, setJammerInfo] = useState({});
     
     useEffect(() => {
         getJammersList(jamId)
@@ -21,26 +21,24 @@ const LandlordTenants = ({ jamId, subSection, setSubSection }) => {
 
     const getJammersList = async (jamId) => {
         const res = await DataService.getJammers(jamId);
-        res.length > 0 && setShowJammersList(true);
-        setJammers(res);
-        const defaultTenant = res[0].id;
-        setSubSection(defaultTenant)
+        if(res.length > 0) {
+            // setShowJammersList(true);
+            const organizedTenants = Calculations.organizeTenants(res);
+            console.log('organizedTenants: ', organizedTenants);
+            setJammers(organizedTenants);
+        };
     };
 
-    useEffect(() => {
-        if (subSection !== '') {
-            getJammerInfo(jamId, subSection);
-        }
-    }, [subSection]);
+    // useEffect(() => {
+    //     if (subSection !== '') {
+    //         getJammerInfo(jamId, subSection);
+    //     }
+    // }, [subSection]);
 
-    const getJammerInfo = async (jamId) => {
-        const res = await DataService.getJammerInfo(jamId, subSection);
-        setJammerInfo(res);
-    };
-
-    const currentTenants = [];
-    const comingTenants = [];
-    const formerTenants = [];
+    // const getJammerInfo = async (jamId) => {
+    //     const res = await DataService.getJammerInfo(jamId, subSection);
+    //     setJammerInfo(res);
+    // };
 
 
     return (
@@ -51,16 +49,16 @@ const LandlordTenants = ({ jamId, subSection, setSubSection }) => {
                     <p>Current Tenants</p>
                 </div>
                 <div className="subsection-wrapper">
-                    <LandlordTenantsList tenantsList={currentTenants} />
+                    <LandlordTenantsList tenantsList={jammers.currentTenants} />
                 </div>
             </div>
 
             <div className="landlord-tenants-section">
                 <div className="subSection-title">
-                    <p>Coming Tenants</p>
+                    <p>Future Tenants</p>
                 </div>
                 <div className="subsection-wrapper">
-                    <LandlordTenantsList tenantsList={comingTenants} />
+                    <LandlordTenantsList tenantsList={jammers.futureTenants} />
                 </div>
             </div>
 
@@ -69,7 +67,7 @@ const LandlordTenants = ({ jamId, subSection, setSubSection }) => {
                     <p>Former Tenants</p>
                 </div>
                 <div className="subsection-wrapper">
-                    <LandlordTenantsList tenantsList={formerTenants} />
+                    <LandlordTenantsList tenantsList={jammers.formerTenants} />
                 </div>
             </div>
             
@@ -79,18 +77,9 @@ const LandlordTenants = ({ jamId, subSection, setSubSection }) => {
 };
 
 
-// const mapStateToProps = (state) => {
-//     return {
-//         user: state.firebase.auth,
-//         jamId: state.jamId,
-//         jamActiveSection: state.jamSection,
-//         jammerId: state.jammerId
-//     }
-// }
-
 const mapStateToProps = (state) => {
     const { subSection } = state.nav;
     return { subSection }
     
 };
-export default connect(mapStateToProps, { setSubSection })(LandlordTenants);
+export default connect(mapStateToProps, null)(LandlordTenants);
