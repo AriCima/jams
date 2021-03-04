22e25fb33a7c1791bb0fb14a81ee2e2de8fde531
Ariel Cima <arielcimax@gmail.comt>
2021-03-04 13:16:58 +0100

    rents verification

--- src/Components/JamSections/Overview/AdminOverview/RentPayments/index.js
+++ src/Components/JamSections/Overview/AdminOverview/RentPayments/index.js
@@ -20,10 +20,37 @@ const RentPaymentsInfo = ({ jamId, rooms, jammers, jamDetails }) => {
 
     for (let i = 0; i < cTenants.length; i++) {
         const { userId, rentsArray, firstName, lastName, roomNr } = cTenants[i];
+        // const teoricalRoomRent = rooms.filter(e => e.roomNr).rent;
         const currentMonthRent = rentsArray[cM];
-        const tenantPayment = {firstName, lastName, roomNr, userId, currentMonthRent}
+        const tenantPayment = {firstName, lastName, roomNr, userId, currentMonthRent};
         currentPayments.push(tenantPayment);
     };
+
+    const takeMeToTenantInfo
+
+    const renderTenantsPayments = () => currentPayments.map(((cP, userId) => {
+        
+        if (cp.currentMonthRent.paid) return;
+
+        return (
+            <div
+                className="rent-payment-line"
+                key={userId}
+                onCLick={takeMeToTenantInfo(cp.userId)}
+            >
+                <div className="rent-payment-block">
+                    {cP.firstName} {cP.lastName}
+                </div>
+                <div className="rent-payment-block">
+                    {cp.roomNr}
+                </div>
+                <div className="rent-payment-block">
+                    {cp.currentMonthRent.rent}
+                </div>
+            </div>
+        )
+    }
+    )
 
     return (
         <div className="rent-payments-wrapper">
